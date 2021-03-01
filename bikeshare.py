ac8d923f20ca62c3fb2b1eda1c924e95bd2e116b
Rahaf Alzahrani <rz.ud@hotmail.com>
2021-03-01 03:46:45 +0300

    Adding A Thank You message in the end

--- bikeshare.py
+++ bikeshare.py
@@ -206,6 +206,7 @@ def main():
             restart = input('\nWould you like to restart? Enter yes or no.\n')
         if restart.lower() != 'yes':
             break
+    print('Thank you for using our program')
 
 
 
